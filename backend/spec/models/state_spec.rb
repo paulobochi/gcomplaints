9ec84cc17fe3f98a692968bd9c67ba59cbbd6d1d
Paulo Medeiros <pauloricardodemedeiros@hotmail.com>
2019-06-01 23:40:52 -0300

    Add state Model and Tests

--- backend/spec/models/state_spec.rb
+++ backend/spec/models/state_spec.rb
@@ -1,12 +1,12 @@
 require 'rails_helper'
 
-RSpec.describe Country, type: :model do
+RSpec.describe State, type: :model do
   describe "Validations" do
     it { should validate_presence_of(:name) }
     it { validate_uniqueness_of(:name) }
   end
 
   describe "Associations" do
-    it { should have_many(:states) }
+    it { should have_many(:cities) }
   end
 end
